333556d6abdceed604fec69d1def40e26e19dad5
Sergiy Voznyak <sergiy.voznyak@gmail.com>
2018-10-01 21:18:47 +0200

    Fix version

--- server-version.js
+++ server-version.js
@@ -1,9 +1,11 @@
-module.exports.handler = () => ({
-  statusCode: 200,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-  body: JSON.stringify({
-    version: '1.0.0',
-  }),
-});
+module.exports.handler = (event, context, callback) => (
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      version: '1.0.0',
+    }),
+  })
+);
